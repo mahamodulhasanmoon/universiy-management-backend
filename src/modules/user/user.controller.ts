92af8224c6d842d76a7df02af37e749611ff5cfd
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-26 23:52:31 +0600

    fix proper file catchAsync functioon

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,9 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { RequestHandler } from 'express';
 import { createStudentService } from './user.service';
 import { sendResponse } from '../../utils/sendResponse';
+import { catchAsync } from '../../utils/catchAsync';
 
-const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(error => next(error));
-  };
-};
+
 
 export const createStudentController: RequestHandler = catchAsync(
   async (req, res) => {
