9673755ca34b269728c026d79c4d25f82f26cd4d
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-30 00:06:44 +0600

    done  remove console.log

--- src/middlewares/requestValidator.ts
+++ src/middlewares/requestValidator.ts
@@ -4,7 +4,6 @@ import { AnyZodObject } from 'zod';
 export const requestValidator = (schema: AnyZodObject) => {
   return async (req: Request, _res: Response, next: NextFunction) => {
     try {
-      console.log(req.body);
       await schema.parseAsync({
         body : req.body,
       });
