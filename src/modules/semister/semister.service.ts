c15f050aa9070f87c59a296f9630f7315025be52
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-30 09:59:00 +0600

    semister code added

--- src/modules/semister/semister.service.ts
+++ src/modules/semister/semister.service.ts
@@ -1,8 +1,24 @@
 import { ISemister } from "./semister.interface";
 import { Semester } from "./semister.model";
 
-export const createSemisterService = async (data: ISemister) => {
-    const result = await Semester.create(data);
+type ISemesterNameCodeWrapper ={
+    [key: string]: string
+}
+
+
+
+export const createSemisterService = async (payload: ISemister) => {
+const semisterNameCodeWrapper:ISemesterNameCodeWrapper = {
+    Autumn:'A01',
+    Summer:'S02',
+    Fall:'F03'
+
+}
+if(semisterNameCodeWrapper[payload.name] !== payload.code){
+   throw new Error('invalid semister Code   ' + payload.name)
+}
+
+    const result = await Semester.create(payload);
     return result;
   };
   
