d31470a7e1273df534557166769a3a82988ee196
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-31 10:52:50 +0600

    system perfectly working

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -1,33 +1,48 @@
 import { ISemister } from '../semister/semister.interface';
 import { User } from './user.model';
 
+const findLastStudentId = async (payload:ISemister) => {
+  const lastStudent = await User.findOne(
+    {
+      role: 'student',
+      id: { $regex: `${payload.code}${payload.year}-\\d{4}$` }
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
 
-const findLastStudentId = async () => {
-    const lastStudent = await User.findOne(
-      {
-        role: 'student',
-      },
-      {
-        id: 1,
-        _id: 0,
-      },
-    )
-      .sort({
-        createdAt: -1,
-      })
-      .lean();
+  //203001   0001
 
-    //203001   0001
-
-    // eslint-disable-next-line no-undefined
-    return lastStudent?.id ? lastStudent.id.substring(8) : undefined;
-  };
+  // eslint-disable-next-line no-undefined
+  return lastStudent?.id ? lastStudent.id : undefined;
+};
 
 export const genarateStudentID = async (payload: ISemister) => {
-  const currentID = (await findLastStudentId()) || (0);
+  let currentID = (0).toString();
+  const lastStudentID = await findLastStudentId(payload);
+  const lastSemisterCode = lastStudentID?.substring(0, 3);
+  const lastSemisterYear = lastStudentID?.substring(3, 7);
+
+  // currentInfo
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentID &&
+    lastSemisterCode === currentSemesterCode &&
+    lastSemisterYear === currentYear
+  ) {
+    currentID = lastStudentID?.substring(8)
+  }
+
+
   const incrementID = (+currentID + 1).toString().padStart(4, '0');
-
-
 
   const incrementalId = `${payload.code}${payload.year}-${incrementID}`;
   return incrementalId;
