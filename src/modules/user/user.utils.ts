30d61dfa5f8c5592d4c9f83fa0003455b5f8cce8
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-05-31 10:14:42 +0600

    id initial solved

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -16,37 +16,19 @@ const findLastStudentId = async () => {
         createdAt: -1,
       })
       .lean();
-  
+
     //203001   0001
- 
+
     // eslint-disable-next-line no-undefined
-    return lastStudent?.id ? lastStudent.id.substring(4) : undefined;
+    return lastStudent?.id ? lastStudent.id.substring(8) : undefined;
   };
 
 export const genarateStudentID = async (payload: ISemister) => {
+  const currentID = (await findLastStudentId()) || (0);
+  const incrementID = (+currentID + 1).toString().padStart(4, '0');
 
-  // first time 0000
-  //0001  => 1
-  let currentId = (0).toString(); // 0000 by deafult
 
-  const lastStudentId = await findLastStudentId();
-  // 2030 01 0001
-  const lastStudentSemesterCode = lastStudentId?.substring(5, 8); //01;
-  const lastStudentYear = lastStudentId?.substring(0,4); // 2030
-  const currentSemesterCode = payload.code;
-  const currentYear = payload.year;
-  console.log(lastStudentId, currentSemesterCode, currentYear);
-  if (
-    lastStudentId &&
-    lastStudentSemesterCode === currentSemesterCode &&
-    lastStudentYear === currentYear
-  ) {
-    currentId = lastStudentId.substring(8); // 00001
-  }
 
-  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
-
-  incrementId = `${payload.year}${payload.code}${incrementId}`;
-
-  return incrementId;
+  const incrementalId = `${payload.code}${payload.year}-${incrementID}`;
+  return incrementalId;
 };
